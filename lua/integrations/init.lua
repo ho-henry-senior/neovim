c79484759d0fa57145a773d5ac25b2322e13b17b
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2026-06-21 22:18:38 +0100

    add comment explaining the ./integrations directory

--- lua/integrations/init.lua
+++ lua/integrations/init.lua
@@ -1,3 +1,5 @@
+-- The `./integrations` directory is a Lua module that serves as a centralized location for configuring and managing various integrations within a Neovim setup.
+-- It allows users to easily set up and customize different tools that enhance the functionality of Neovim.
 local M = {}
 
 function M.setup()
