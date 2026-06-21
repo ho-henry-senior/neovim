94fb6dbed747ccd25212e5a63b7ae0ffdc2da7da
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2026-06-21 19:52:03 +0100

    highlight active window

--- lua/config/options/ui.lua
+++ lua/config/options/ui.lua
@@ -1,4 +1,50 @@
+local augroup = require("config.util").augroup
 local opt = vim.opt
+
+local function get_hl(name)
+	return vim.api.nvim_get_hl(0, { name = name, link = false })
+end
+
+local function blend_channel(from, to, alpha)
+	return math.floor((from * (1 - alpha)) + (to * alpha) + 0.5)
+end
+
+local function blend_colors(from, to, alpha)
+	local from_r = math.floor(from / 0x10000) % 0x100
+	local from_g = math.floor(from / 0x100) % 0x100
+	local from_b = from % 0x100
+	local to_r = math.floor(to / 0x10000) % 0x100
+	local to_g = math.floor(to / 0x100) % 0x100
+	local to_b = to % 0x100
+
+	return (blend_channel(from_r, to_r, alpha) * 0x10000)
+		+ (blend_channel(from_g, to_g, alpha) * 0x100)
+		+ blend_channel(from_b, to_b, alpha)
+end
+
+local function apply_inactive_window_highlights()
+	local normal = get_hl("Normal")
+	local comment = get_hl("Comment")
+	local line_nr = get_hl("LineNr")
+	local muted_fg = comment.fg or line_nr.fg or normal.fg
+	local inactive_bg = (normal.bg and muted_fg) and blend_colors(normal.bg, muted_fg, 0.12) or normal.bg
+
+	if not muted_fg then
+		return
+	end
+
+	vim.api.nvim_set_hl(0, "NormalNC", {
+		bg = inactive_bg,
+		fg = normal.fg,
+	})
+	vim.api.nvim_set_hl(0, "SignColumnNC", {
+		bg = inactive_bg,
+	})
+	vim.api.nvim_set_hl(0, "LineNrNC", {
+		bg = inactive_bg,
+		fg = line_nr.fg or muted_fg,
+	})
+end
 
 opt.number = true -- Line numbers
 opt.relativenumber = true -- Relative line numbers
@@ -33,3 +79,12 @@ opt.laststatus = 3 -- global statusline
 opt.linebreak = true -- Wrap lines at convenient points
 opt.list = false -- Show some invisible characters (tabs...)
 opt.shortmess:append({ W = true, I = true, c = true, C = true })
+
+apply_inactive_window_highlights()
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+	group = augroup("inactive_window_highlights"),
+	callback = function()
+		vim.schedule(apply_inactive_window_highlights)
+	end,
+})
