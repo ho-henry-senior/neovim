d42c6fc7b3d99a54248cfe04d9014c7de0eb2bb2
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2026-07-29 12:50:59 +0100

    fix for marksman errors

--- lsp/marksman_ls.lua
+++ lsp/marksman_ls.lua
@@ -1,6 +1,9 @@
 -- Install with: brew install marksman
 return {
 	cmd = { "marksman", "server" },
+	cmd_env = {
+		DOTNET_ROOT_ARM64 = "/opt/homebrew/opt/dotnet@9/libexec",
+	},
 	filetypes = { "markdown" },
 	root_markers = { ".marksman.toml", ".git" },
 	single_file_support = true,
